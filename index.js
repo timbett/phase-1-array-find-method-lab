9a5d0b67e4ef548ddeee0f2c3e9127fa17dc312a
Timothy <tkkibet13@gmail.com>
2022-08-22 14:43:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code your solution here
+function superbowlWin(record) {
+    const result = record.find(record => record.result === "W");
+    if(!result){
+      return undefined
+    }
+    return result.year
+  }
